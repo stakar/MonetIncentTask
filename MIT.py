758643db2b68a8cc7a482d68f5a18cb5648c2023
Stanislaw Karkosz <stakarkosz@gmail.com>
2019-07-01 10:45:23 +0200

    Added script with description

---
+++ MIT.py
@@ -0,0 +1,25 @@
+"""
+Monetary Incentive Task
+
+
+This script is being developed for PLAN experimental purposes. I is performing Monetary Incentive Task, measuring time reactions and percent of accurate responses.
+
+
+
+------------
+| Workflow |
+------------
+
+Modules:
+    1.Pre-trial
+    Stimuli is presented on the screen
+    In this module, the respondent is instructed to response as fast as possible for stimuli displayed on the screen.
+    The mean time reaction is obtained.
+    2.Trial
+    The stimuli is displayed on screen 50% times longer than mean time reaction from pre-trial and shorter in 50% of cases, after this time it
+    disappears. After respondent's reaction the feedback is provided about accuracy.
+    Respondent is instructed to react for stimuli before it deisappears.
+    Reaction times are obtained.
+
+
+"""
