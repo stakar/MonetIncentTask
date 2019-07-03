8557ab36ef18e50b1e4cd2d761b623281d350b90
Stanislaw Karkosz <stakarkosz@gmail.com>
2019-07-03 10:26:17 +0200

    Minor changes

--- MIT.py
+++ MIT.py
@@ -20,6 +20,99 @@ Modules:
     disappears. After respondent's reaction the feedback is provided about accuracy.
     Respondent is instructed to react for stimuli before it deisappears.
     Reaction times are obtained.
+    
+TODO:
+    1.Pre-trial
+    2.Trial
+    3.Instructions
+    4.mechanism for calculating response time
 
 
 """
+
+
+
+###########
+# Modules #
+###########
+
+from psychopy import core, visual, gui, data, event
+from psychopy.tools.filetools import fromFile, toFile
+import numpy as np 
+import threading
+import random
+import time
+from psychopy.hardware import keyboard
+
+
+##################
+# INITIALIZATION #
+##################
+
+# Store info about the experiment session
+psychopyVersion = '3.1.2'
+expName = 'PLAN_Experiment'  # from the Builder filename that created this script
+expInfo = {'participant': '', 'session': '001'}
+#dlg = gui.DlgFromDict(dictionary=expInfo, sortKeys=False, title=expName)
+#if dlg.OK == False:
+#    core.quit()  # user pressed cancel
+#expInfo['date'] = data.getDateStr()  # add a simple timestamp
+#expInfo['expName'] = expName
+#expInfo['psychopyVersion'] = psychopyVersion
+
+
+
+win = visual.Window([800,600], monitor="testMonitor", units="deg")
+
+
+
+
+
+#############
+# Pre-trial #
+#############
+
+stim = visual.Rect(
+    win=win,
+    units = "pix",
+    width = 300,
+    height = 300,
+    pos = [0,0],
+    fillColor = [0,1,0]
+)
+start = time.time()
+response_times = []
+#def myfunc():
+ #   pass
+
+#event.globalKeys.add(key='space',func=myfunc)
+
+keyResp = keyboard.Keyboard()
+ 
+
+for n in range(5):
+    time.sleep(random.randint(1,2))
+    stim.draw()
+    win.flip()
+    win.callOnFlip(keyResp.clock.reset)
+    zegarek = core.Clock()
+    time.sleep(random.random())
+    win.clearBuffer()
+    win.flip()
+    
+    theseKeys = keyResp.getKeys(keyList=['space'],waitRelease=False)
+    
+#    if 'space' in theseKeys:
+        
+    print(theseKeys.__len__() )
+    try:
+        print(theseKeys[0].rt)
+    except:
+        print("No touchin\'")
+
+win.clearBuffer()
+win.flip()
+
+core.wait(1.0) 
+
+win.close()
